bd87c5c059b48f436c40e300b5d12b7e9b7aa099
Anton <antonstromkarl@gmail.com>
2019-09-12 22:17:11 +0200

    changed project structure

--- src/App.js
+++ src/App.js
@@ -1,62 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
-import People from './components/people/people';
-import axios from 'axios';
+import ContactList from './containers/ContactList/ContactList';
 
-class App extends Component {
-	state = {
-		people: [],
-		search: '',
-	};
-
-	componentDidMount() {
-		axios.get('http://localhost:3001/api/people').then(response => {
-			this.setState({ people: response.data });
-		});
-	}
-
-	filterPeople(e) {
-		e.preventDefault();
-		console.log(e.target.value);
-		this.setState({ search: e.target.value });
-	}
-
-	renderPeople = () => {
-		const peopleToRender = this.state.people
-			.filter(
-				p =>
-					p.name.firstName.includes(this.state.search) ||
-					p.name.lastName.includes(this.state.search),
-			)
-			.map(p => (
-				<People
-					key={p._id}
-					person={p}
-					removePerson={this.removePerson}
-				></People>
-			));
-		return peopleToRender;
-	};
-
-	removePerson = id => {
-		const updatedContacts = this.state.people.filter(
-			person => person._id !== id,
-		);
-		axios
-			.delete(`http://localhost:3001/api/people/${id}`)
-			.then(response => console.log(response.data));
-		this.setState({ people: updatedContacts });
-	};
-
-	render() {
-		return (
-			<div className='App'>
-				<h4>My Contacts</h4>
-				<input placeholder="search..." onKeyUp={e => this.filterPeople(e)}></input>
-				<div className='contactRow'>{this.renderPeople()}</div>
-			</div>
-		);
-	}
-}
+const App = () => {
+	return (
+		<div className='App'>
+			<ContactList />
+		</div>
+	);
+};
 
 export default App;
---
+++ src/containers/ContactList/ContactList.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import People from '../../components/people/people';
+import axios from 'axios';
+import styles from './ContactList.module.css';
+
+class ContactList extends Component {
+	state = {
+		people: [],
+		search: '',
+	};
+
+	componentDidMount() {
+		axios.get('http://localhost:3001/api/people').then(response => {
+			this.setState({ people: response.data });
+		});
+	}
+
+	filterPeople(e) {
+		e.preventDefault();
+		console.log(e.target.value);
+		this.setState({ search: e.target.value });
+	}
+
+	renderPeople = () => {
+		const peopleToRender = this.state.people
+			.filter(
+				p =>
+					p.name.firstName.includes(this.state.search) ||
+					p.name.lastName.includes(this.state.search),
+			)
+			.map(p => (
+				<People
+					key={p._id}
+					person={p}
+					removePerson={this.removePerson}
+				></People>
+			));
+		return peopleToRender;
+	};
+
+	removePerson = id => {
+		const updatedContacts = this.state.people.filter(
+			person => person._id !== id,
+		);
+		axios
+			.delete(`http://localhost:3001/api/people/${id}`)
+			.then(response => console.log(response.data));
+		this.setState({ people: updatedContacts });
+	};
+
+	render() {
+		return (
+			<>
+				<h4>My Contacts</h4>
+				<input
+					placeholder='search...'
+					onKeyUp={e => this.filterPeople(e)}
+				></input>
+				<div className={styles.ContactRow}>{this.renderPeople()}</div>
+			</>
+		);
+	}
+}
+
+export default ContactList;
