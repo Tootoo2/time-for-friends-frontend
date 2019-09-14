b58f33d199ed48f32dd933f78565ff6bfcec270b
Anton <antonstromkarl@gmail.com>
2019-09-14 22:53:27 +0200

    added a trash can to remove the person instead of clicking the whole container

--- src/components/People/People.js
+++ src/components/People/People.js
@@ -5,14 +5,15 @@ import style from './People.module.css';
 
 const people = props => {
 	return (
-		<div
-			className={style.contactCard}
-			onClick={() => props.removePerson(props.person._id)}
-		>
+		<div className={style.contactCard}>
+			<Clock timeZone={props.person.location.timeZone} />
 			<h4>
-				{props.person.name.firstName} {props.person.name.lastName} <br />
-				<Clock timeZone={props.person.location.timeZone}/>
+				{props.person.name.firstName} {props.person.name.lastName}
 			</h4>
+			<i
+				className='fas fa-trash'
+				onClick={() => props.removePerson(props.person._id)}
+			></i>
 		</div>
 	);
 };
