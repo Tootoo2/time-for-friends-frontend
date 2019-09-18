6018347fb7863b8313b6db637ae88c10fc072113
Anton <antonstromkarl@gmail.com>
2019-09-18 12:00:36 +0200

    removed unused import

--- src/containers/ContactList/ContactList.js
+++ src/containers/ContactList/ContactList.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Route } from 'react-router-dom';
 
 import Contact from '../../components/Contact/Contact';
 import Clock from '../../components/Clock/Clock';
