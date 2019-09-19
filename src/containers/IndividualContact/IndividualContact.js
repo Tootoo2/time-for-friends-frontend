b8a65e7333bd98099f20c64eab54fed2e2f5cd5c
Anton <antonstromkarl@gmail.com>
2019-09-19 09:04:15 +0200

    now displaying more info on induvidual page component

--- src/containers/IndividualContact/IndividualContact.js
+++ src/containers/IndividualContact/IndividualContact.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
 
-const individualContact = (props) => {
-  console.log(props)
-  const contact = props.location.state
-  return (<div>{contact.name.firstName}</div>)
-}
+import styles from './IndividualContact.module.css';
+
+const individualContact = props => {
+	console.log(props);
+	const contact = props.location.state;
+	return (
+		<div className={styles.IndividualContactContainer}>
+			<ul>
+				<li>{contact.name.firstName} {contact.name.lastName}</li>
+        <li>{contact.phoneNumber}</li>
+        <li>{contact.email}</li>
+        <li>{contact.location.timeZone}</li>
+			</ul>
+		</div>
+	);
+};
 
 export default individualContact;
