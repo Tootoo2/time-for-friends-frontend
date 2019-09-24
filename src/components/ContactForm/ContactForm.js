809d4e64f579ac8bf0be48c7e51e2b0e05b63c15
Anton <antonstromkarl@gmail.com>
2019-09-24 23:03:49 +0200

    first try of fixing validation on the front end for submitting new person. Work in progress..

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -18,27 +18,48 @@ class ContactForm extends Component {
 				timeZone: '',
 			},
 		},
+		isValid: {
+			firstName: false,
+			lastName: false,
+			phoneNumber: false,
+			email: false,
+		},
+		touched: {
+			firstName: false,
+			lastName: false,
+			phoneNumber: false,
+			email: false,
+		},
 	};
 
 	updateContactForm = e => {
 		const updatedContact = { ...this.state.contact };
+		this.checkValidity(e.target.name, e.target.value);
 
 		switch (e.target.name) {
 			case 'firstName':
-				updatedContact.name.firstName = e.target.value;
-				this.setState({ contact: updatedContact });
+				if (this.state.isValid.firstName) {
+					updatedContact.name.firstName = e.target.value;
+					this.setState({ contact: updatedContact });
+				}
 				break;
 			case 'lastName':
-				updatedContact.name.lastName = e.target.value;
-				this.setState({ contact: updatedContact });
+				if (this.state.isValid.lastName) {
+					updatedContact.name.lastName = e.target.value;
+					this.setState({ contact: updatedContact });
+				}
 				break;
 			case 'phoneNumber':
-				updatedContact.phoneNumber = e.target.value;
-				this.setState({ contact: updatedContact });
+				if (this.state.isValid.phoneNumber) {
+					updatedContact.phoneNumber = e.target.value;
+					this.setState({ contact: updatedContact });
+				}
 				break;
 			case 'mail':
-				updatedContact.email = e.target.value;
-				this.setState({ contact: updatedContact });
+				if (this.state.isValid.email) {
+					updatedContact.email = e.target.value;
+					this.setState({ contact: updatedContact });
+				}
 				break;
 			case 'country':
 				updatedContact.location.country = e.target.value;
@@ -55,10 +76,54 @@ class ContactForm extends Component {
 			default:
 				console.log('not valid input');
 		}
-		console.log(this.state.contact);
 	};
 
+	checkValidity(inputField, value) {
+		const updateIsValid = { ...this.state.isValid };
+		const isTouched = { ...this.state.touched };
+		const nameRegex = /^[A-za-z][a-z.\s-]{1,15}$/;
+		const phoneRegex = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s./0-9]{5,12}$/;
+		const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+		switch (inputField) {
+			case 'firstName':
+				nameRegex.test(value)
+					? (updateIsValid.firstName = true)
+					: (updateIsValid.firstName = false);
+				isTouched.firstName = true;
+				this.setState({ isValid: updateIsValid, touched: isTouched });
+				break;
+			case 'lastName':
+				nameRegex.test(value)
+					? (updateIsValid.lastName = true)
+					: (updateIsValid.lastName = false);
+				isTouched.lastName = true;
+				this.setState({ isValid: updateIsValid, touched: isTouched });
+				break;
+			case 'phoneNumber':
+				phoneRegex.test(value)
+					? (updateIsValid.phoneNumber = true)
+					: (updateIsValid.phoneNumber = false);
+				isTouched.phoneNumber = true;
+				this.setState({ isValid: updateIsValid, touched: isTouched });
+				break;
+			case 'mail':
+				emailRegex.test(value)
+					? (updateIsValid.email = true)
+					: (updateIsValid.email = false);
+				isTouched.email = true;
+				this.setState({ isValid: updateIsValid, touched: isTouched });
+				break;
+			case 'country':
+				console.log(value);
+				break;
+			default:
+				console.log('something went wrong');
+				break;
+		}
+	}
+
 	submitForm = e => {
+		this.isFormReady();
 		e.preventDefault();
 		axios
 			.post('http://localhost:3001/api/people/addcontact', this.state.contact)
@@ -83,69 +148,88 @@ class ContactForm extends Component {
 			.catch(err => console.error(err));
 	};
 
+	isFormReady() {
+		const arrayOfTruth = Object.values(this.state.isValid).filter(v => v === true);
+		return arrayOfTruth.length === 4 ? true : false;
+	}
+
 	render() {
 		return (
 			<div className={styles.FormContainer}>
-				<form id='test' onSubmit={this.submitForm}>
-					<div>
+				<form className={styles.TheForm} onSubmit={this.submitForm}>
+					<div className={styles.InputContainer}>
 						<input
 							className={styles.InputFields}
 							type='text'
-							value={this.state.contact.name.firstName}
 							name='firstName'
 							placeholder='First Name'
-							onChange={this.updateContactForm}
+							onBlur={this.updateContactForm}
 						/>
+						{!this.state.isValid.firstName && this.state.touched.firstName ? (
+							<p className={styles.InvalidInput}>Invalid name</p>
+						) : null}
+					</div>
+					<div className={styles.InputContainer}>
 						<input
 							className={styles.InputFields}
 							type='text'
-							value={this.state.contact.name.lastName}
 							name='lastName'
 							placeholder='Last Name'
-							onChange={this.updateContactForm}
+							onBlur={this.updateContactForm}
 						/>
+						{!this.state.isValid.lastName && this.state.touched.lastName ? (
+							<p className={styles.InvalidInput}>Invalid name</p>
+						) : null}
+					</div>
+					<div className={styles.InputContainer}>
 						<input
 							className={styles.InputFields}
 							type='text'
-							value={this.state.contact.phoneNumber}
 							name='phoneNumber'
 							placeholder='Phone Number'
-							onChange={this.updateContactForm}
+							onBlur={this.updateContactForm}
 						/>
+						{!this.state.isValid.phoneNumber &&
+						this.state.touched.phoneNumber ? (
+							<p className={styles.InvalidInput}>Invalid number</p>
+						) : null}
+					</div>
+					<div className={styles.InputContainer}>
 						<input
 							className={styles.InputFields}
 							type='text'
-							value={this.state.contact.email}
 							name='mail'
 							placeholder='Mail'
-							onChange={this.updateContactForm}
+							onBlur={this.updateContactForm}
 						/>
-						<input
-							className={styles.InputFields}
-							type='text'
-							value={this.state.contact.location.country}
-							name='country'
-							placeholder='Country'
-							onChange={this.updateContactForm}
-						/>
-						<input
-							className={styles.InputFields}
-							type='text'
-							value={this.state.contact.location.city}
-							name='city'
-							placeholder='City'
-							onChange={this.updateContactForm}
-						/>
-						<input
-							className={styles.InputFields}
-							type='text'
-							value={this.state.contact.location.timeZone}
-							name='timeZone'
-							placeholder='Time Zone'
-							onChange={this.updateContactForm}
-						/>
+						{!this.state.isValid.email && this.state.touched.email ? (
+							<p className={styles.InvalidInput}>Invalid email</p>
+						) : null}
 					</div>
-					<button type='submit'>Submit</button>
+					<select
+						className={styles.InputFields}
+						type='text'
+						name='country'
+						placeholder='Country'
+						onChange={this.updateContactForm}
+					></select>
+					<select
+						className={styles.InputFields}
+						type='text'
+						value={this.state.contact.location.city}
+						name='city'
+						placeholder='City'
+						onChange={this.updateContactForm}
+					></select>
+					<input
+						className={styles.InputFields}
+						type='text'
+						value={this.state.contact.location.timeZone}
+						name='timeZone'
+						placeholder='Time Zone'
+						onChange={this.updateContactForm}
+					/>
+					{this.isFormReady() ? <button type="submit">Submit</button>:<button type='submit' disabled>Submit</button>}
 				</form>
 			</div>
 		);
